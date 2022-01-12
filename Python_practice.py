83755bbe2f5b6d85cbc5f356c4b9877ed72fd2e3
Winting <winting.vincent@mayo.edu>
2022-01-11 23:36:11 -0800

    updates with practice work in module

--- Python_practice.py
+++ Python_practice.py
@@ -11,12 +11,15 @@
 counties = ["Arapahoe","Denver","Jefferson"]
 # if counties[1] == "Denver":
 #     print(counties[1])
-if "El Paso" in counties:
-    print("El Paso is in the list of counties.")
-else:
-    print("El Paso is not in the list of counties.")
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not in the list of counties.")
 
-if "Arapahoe" in counties or "El Paso" in counties:
-    print("Arapahoe and El Paso are in the list of counties.")
-else:
-    print("Arapahoe or El Paso is not in the list of counties.")
+# if "Arapahoe" in counties or "El Paso" in counties:
+#     print("Arapahoe or El Paso are in the list of counties.")
+# else:
+#     print("Arapahoe and El Paso is not in the list of counties.")
+
+for county in counties.keys():
+    print(county)
