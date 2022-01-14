f7aa84538fa044b0e3faa4a2830817bd4e053e2d
Winting <winting.vincent@mayo.edu>
2022-01-13 21:34:17 -0800

    starting point for assignment

---
+++ PyPoll.py
@@ -0,0 +1,9 @@
+#Read in file
+#Set end point
+#Create list of candidates
+# Will Need
+    #Total Number of Votes Cast
+    #Complete list of candidates who received votes
+    #Total number of votes each candidate received
+    #Percentage of votes each candidate won
+    #The winner of the election based on popular vote
